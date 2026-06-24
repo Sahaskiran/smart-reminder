3615a4d16c11e9c5f996fafd9047ec1102d9bd89
Sahaskiran <sahaskiran80@gmail.com>
2026-06-24 22:30:28 +0530

    Update SMTP port to 465 (SSL) for cloud deployment reliability

--- src/mailer.js
+++ src/mailer.js
@@ -22,8 +22,8 @@ function initTransporter() {
 
   transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: { user, pass }
   });
 
